17edd901483bbf6825d5ea4e60d88a301db48948
r-eler <poniloitoto@gmail.com>
2026-03-25 21:28:15 -0300

    feat: Configuraçao do tailwind e plugin prettier

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+};
