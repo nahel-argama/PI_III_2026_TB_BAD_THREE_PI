d67b8df35a57d005f588a56a77f4679d4bdfd09f
r-eler <poniloitoto@gmail.com>
2026-03-26 21:16:45 -0300

    feat: Configuraçao do tailwind e plugin prettier (#50)
    * feat: Configuraçao do tailwind e plugin prettier
    
    * refactor: correçao de letras minusculas e lang

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,8 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+  <h1 class="= text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <style scoped></style>
