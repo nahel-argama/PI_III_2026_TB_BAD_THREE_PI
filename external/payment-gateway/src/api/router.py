9ef85469c71f45529681842c227a8159f81e3984
ngracioli <nicolas.gracioli@gmail.com>
2026-04-15 21:12:18 -0300

    fix(payment-gateway): remove unnecessary blank line in router.py

--- external/payment-gateway/src/api/router.py
+++ external/payment-gateway/src/api/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from src.api.routes import HealthRoute, PaymentsRoute
 
-
 api_router = APIRouter()
 api_router.include_router(HealthRoute)
 api_router.include_router(PaymentsRoute)
