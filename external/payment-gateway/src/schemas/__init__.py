668282f03c8fd8b65d366358df0df880b3c783cc
ngracioli <nicolas.gracioli@gmail.com>
2026-04-15 21:12:53 -0300

    feat(payment-gateway): clean up schemas and add empty init files for services and tests

--- external/payment-gateway/src/schemas/__init__.py
+++ external/payment-gateway/src/schemas/__init__.py
@@ -1,15 +1,12 @@
 from .payment.credit_card.schema import CardSchema
-from .payment.customer.schema import CustomerSchema
 from .payment.request_schema import PaymentRequestSchema
-from .payment.response_schema import PaymentResponseSchema, PaymentResponseDataSchema
+from .payment.response_schema import PaymentResponseSchema
 from .payment.enums import PaymentMethods, PaymentStatus
 
 __all__ = [
     "CardSchema",
-    "CustomerSchema",
     "PaymentRequestSchema",
     "PaymentResponseSchema",
-    "PaymentResponseDataSchema",
     "PaymentMethods",
     "PaymentStatus",
 ]
