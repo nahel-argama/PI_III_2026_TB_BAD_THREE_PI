f3cb075545815a024ec71ee1ceba51ee51e91bfc
henriqueoz <pedroozorio30@gmail.com>
2026-05-18 21:45:46 -0300

    feat: add external product fetching in default image upload

--- backend/default_product_image/views.py
+++ backend/default_product_image/views.py
@@ -1,10 +1,12 @@
-from rest_framework import status
+from rest_framework import exceptions, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.db import transaction
 
+from product import price_scrapper_client
+from product.price_scrapper_client import ExternalServiceError
 from default_product_image.models import DefaultProductImage
 from default_product_image.serializers import (
     DefaultProductImageSerializer,
@@ -49,7 +51,8 @@ class DefaultProductImageUploadView(APIView):
         serializer.is_valid(raise_exception=True)
 
         upload = serializer.validated_data["image"]
-        product_name = serializer.validated_data["product_name"]
+        external_response = self.get_product_by_external_key(product_external_key)
+        product_name = external_response["name"]
 
         default_image = self.store_new_image(product_name, product_external_key, upload)
 
@@ -61,7 +64,7 @@ class DefaultProductImageUploadView(APIView):
 
     def store_new_image(self, product_name, product_external_key, upload):
         with transaction.atomic():
-            default_image, _ = DefaultProductImage.objects.get_or_create(
+            default_image, was_created = DefaultProductImage.objects.get_or_create(
                 product_external_key=product_external_key,
                 defaults={"product_name": product_name}
             )
@@ -76,9 +79,27 @@ class DefaultProductImageUploadView(APIView):
             default_image.save(update_fields=["image", "product_name"])
 
             if not old_image:
-                return
+                return default_image
 
             if old_image.product_images.exists():
-                return
+                return default_image
 
             old_image.delete()
+
+        return default_image
+
+    def get_product_by_external_key(self, product_external_key):
+        response = price_scrapper_client.get_product_by_id(product_external_key)
+
+        if isinstance(response, ExternalServiceError):
+            raise exceptions.APIException(
+                detail=response.message, code=status.HTTP_502_BAD_GATEWAY
+            )
+
+        if response.status != 200:
+            raise exceptions.APIException(
+                detail="Failed to retrieve product information from external service.",
+                code=status.HTTP_502_BAD_GATEWAY,
+            )
+
+        return response.data
